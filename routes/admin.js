e9b55e3a57c6d09b92ce6f694de96725ebe8cad2
Amit Adhikari <amitadhikari525252@gmail.com>
2023-07-16 13:55:04 +0530

    Serving HTML , CSS and JS (VIEWS)

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,14 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
-router.get('/add-product', (req, res, next) => {
-    res.send(
-        '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-    );
+router.get('/contactus', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'contactus.html'));
 });
 
-router.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
+router.post('/success', (req, res, next) => {
+    res.send('<h1>Form successfully filled</h1>');
 });
 
 module.exports = router;
+
